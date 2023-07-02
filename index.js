5d6c900d7b672be0a0b7df937c9b18482f6f94ba
Sachayanthan <sachayanthan.v.1999@gmail.com>
2023-07-02 19:29:17 +0530

    commit for before start of authentication

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// root file
+const express = requrie('express');
+const app = express();
+const PORT = 8000;
+
+
+app.get('/', function(req, res){
+    res.send('<h1>App is working fine</h1>');
+});
+
+app.listen(PORT, function(err) {
+    if(err){
+        console.log(`Error while connecting to the PORT :: ${PORT},\n\n ${err}`);
+    }
+    console.log(`Successfully connected to the PORT :: ${PORT}`);
+});
