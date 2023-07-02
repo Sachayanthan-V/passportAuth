7c437fa900345c224aab96bd567855bd270ae50e
Sachayanthan <sachayanthan.v.1999@gmail.com>
2023-07-02 19:30:11 +0530

    Full version to start implement  authentication process

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const express = requrie('express');
+const express = require('express');
 const app = express();
-const PORT = 8000;
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
-app.get('/', function(req, res){
-    res.send('<h1>App is working fine</h1>');
-});
+// use express router
+app.use('/', require('./routes'));
 
-app.listen(PORT, function(err) {
-    if(err){
-        console.log(`Error while connecting to the PORT :: ${PORT},\n\n ${err}`);
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log(`Successfully connected to the PORT :: ${PORT}`);
+
+    console.log(`Server is running on port: ${port}`);
 });
